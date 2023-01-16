71c9a81dfa77c97e143a24484ca10299c2def2a8
mazinalismaili <mazin.alismaili@gmail.com>
2023-01-16 23:57:32 +0400

    adding list to Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,89 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MaterialApp(
+  runApp(homePage());
+}
+
+class homePage extends StatefulWidget {
+  const homePage({super.key});
+
+  @override
+  State<homePage> createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+
+  final List<String> entries = [];
+  final textController = TextEditingController();
+
+
+  @override
+  Widget build(BuildContext context) {
+    entries.sort();
+    return MaterialApp(
+
+    debugShowCheckedModeBanner: false,
+
+    theme: ThemeData(brightness: Brightness.light),
+    darkTheme: ThemeData(brightness: Brightness.dark),
+    themeMode: ThemeMode.system,
+
     home: Scaffold(
       appBar: AppBar(
-        title: const Center(
-          child: Text("Oman Tour"),
+        title: TextField( controller: textController,),
+
+        leading: Icon(Icons.person),
+        leadingWidth: 56,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.list),
+            tooltip: 'more options',
+            onPressed: () {
+              // handle the press
+            },
           ),
-          backgroundColor: Colors.deepOrange[100],
+        ],
+        backgroundColor: Colors.deepOrange[100],
+          ),
+      body: entries.length > 0 ? ListView.builder(
+        itemCount: entries.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text('${entries[index]}'),
+          );
+        })
+      : Center(
+        child: Text("Your list is empty."),
+        ),
+      
+      drawer: Drawer(
+        child: ListView(
+              //shrinkWrap: true,
+              children: const <Widget>[
+                ListTile(
+                  leading: Icon(Icons.add),
+                  title: Text("Add"),
+                ),
+                ListTile(
+                  leading: Icon(Icons.delete),
+                  title: Text("Delete"),
+                ),
+                ListTile(
+                  leading: Icon(Icons.system_update),
+                  title: Text("Modify"),
+                ),
+              ],
+          )
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.red[900],
+          child: Icon(Icons.add),
+          onPressed: (){
+            setState(() {
+              entries.add(textController.text);
+              textController.clear();
+            });
+          },
           ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed, // Fixed 
@@ -24,8 +100,8 @@ void main() {
             label: "Search",
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: "Profile",
+            icon: Icon(Icons.favorite),
+            label: "Favorite",
           ),
 
           BottomNavigationBarItem(
@@ -36,7 +112,7 @@ void main() {
         ],
 
       ),
-      ),
-    
-  ),);
+      ), 
+  );
+  }
 }
